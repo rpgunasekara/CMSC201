057c628336228e9796581eccb864c1fc66f6009c
rpgunasekara <86504919+rpgunasekara@users.noreply.github.com>
2023-12-29 20:09:50 -0500

    Create hw0.py
    Added an assignment.

---
+++ homeworks/hw0.py
@@ -0,0 +1,9 @@
+"""
+File: hw0.py
+Author: Ravindu Gunasekara
+Date: 08/31/2023
+Section: 40
+E-mail: rgunase1@umbc.edu
+Description: This file is to help students practice using the submission system on the GL servers for CMSC 201.
+"""
+print("This is going to be a great semester!")
